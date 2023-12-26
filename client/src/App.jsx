174e528055892f1162027859efcb3e972e3a41a8
Gun <cobaltgun2324@gmail.com>
2023-12-26 13:49:48 +0700

    feat: fix push assignment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,55 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [blogPost, setBlogPost] = useState([]);
+  const getBlogPost = async () => {
+    const result = await axios.get("http://localhost:4001/products");
+    console.log(result);
+    setBlogPost(result.data.data);
+  };
+  useEffect(() => {
+    getBlogPost();
+  }, [blogPost]);
+  const deleteBlogPost = async (id) => {
+    await axios.delete(`http://localhost:4001/products/${id}`);
+  };
+
   return (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
       <div className="product-list">
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
-          </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
+        {blogPost.map((post, index) => {
+          return (
+            <div className="product" key={index}>
+              <div className="product-preview">
+                <img
+                  src={post.image}
+                  alt="some product"
+                  width="350"
+                  height="350"
+                />
+              </div>
+              <div className="product-detail">
+                <h1>Product name: {post.name}</h1>
+                <h2>Product price: {post.price}</h2>
+                <p>Product description:{post.description}</p>
+              </div>
 
-          <button className="delete-button">x</button>
-        </div>
+              <button
+                className="delete-button"
+                onClick={() => {
+                  deleteBlogPost(post.id);
+                }}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
